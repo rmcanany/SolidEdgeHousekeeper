9dba269214155e1c67904054a29ca005c406356d
Francesco <francesco.arfilli@gmail.com>
2024-10-08 12:24:51 +0200

    Show\Hide property columns

--- My Project/UtilsFileList.vb
+++ My Project/UtilsFileList.vb
@@ -337,8 +337,15 @@ Public Class UtilsFileList
 
                             'Adding extra properties data if needed
                             For Each PropName In Form_Main.ListOfColumns
-                                tmpLVItem.SubItems.Add(FindProp(PropName, FoundFile))
+
+                                If PropName.Name <> "Name" And PropName.Name <> "Path" Then
+
+                                    tmpLVItem.SubItems.Add(FindProp(PropName.Name, FoundFile))
+
+                                End If
+
                             Next
+
 
 
                             tmpLVItem.ImageKey = "Unchecked"
@@ -364,14 +371,29 @@ Public Class UtilsFileList
                 Loop
             End If
 
-            'Creating necessary the columns
+            'Creating necessary the columns     ########################### Unify with line 990
             For Each PropName In Form_Main.ListOfColumns
-                ListViewFiles.Columns.Add(PropName, 50)
+
+                If PropName.Name <> "Name" And PropName.Name <> "Path" Then
+                    ListViewFiles.Columns.Add(PropName.Name, 0)
+                    If PropName.Visible Then ListViewFiles.Columns.Item(ListViewFiles.Columns.Count - 1).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                ElseIf PropName.Name = "Name" Then
+                    If Not PropName.Visible Then
+                        ListViewFiles.Columns.Item(0).Width = 0
+                    Else
+                        ListViewFiles.Columns.Item(0).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                    End If
+                ElseIf PropName.Name = "Path" Then
+                    If Not PropName.Visible Then
+                        ListViewFiles.Columns.Item(1).Width = 0
+                    Else
+                        ListViewFiles.Columns.Item(1).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                    End If
+                End If
+
             Next
 
 
-
-            ListViewFiles.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.ColumnContent)
 
             ListViewFiles.EndUpdate()
 
@@ -931,7 +953,8 @@ Public Class UtilsFileList
 
     Public Sub UpdatePropertiesColumns() '####### To be moved in UtilsFileList
 
-        FMain.Cursor = Cursors.WaitCursor
+        FMain.Cursor.Current = Cursors.WaitCursor
+        Application.DoEvents()
 
         'Resetting the columns
         If ListViewFiles.Columns.Count > 2 Then
@@ -940,10 +963,6 @@ Public Class UtilsFileList
             Loop
         End If
 
-        'Creating necessary the columns
-        For Each PropName In FMain.ListOfColumns
-            ListViewFiles.Columns.Add(PropName, 50)
-        Next
 
 
 
@@ -961,13 +980,45 @@ Public Class UtilsFileList
 
             For Each PropName In FMain.ListOfColumns
 
-                If IO.File.Exists(tmpLVItem.SubItems.Item(0).Name) Then
-                    tmpLVItem.SubItems.Add(UtilsFileList.FindProp(PropName, tmpLVItem.SubItems.Item(0).Name))
+                If PropName.Name <> "Name" And PropName.Name <> "Path" Then
+
+                    If IO.File.Exists(tmpLVItem.SubItems.Item(0).Name) Then
+                        tmpLVItem.SubItems.Add(UtilsFileList.FindProp(PropName.Name, tmpLVItem.SubItems.Item(0).Name))
+                    End If
+
                 End If
 
             Next
 
         Next
+
+
+
+        'Creating necessary the columns
+        For Each PropName In Form_Main.ListOfColumns
+
+            If PropName.Name <> "Name" And PropName.Name <> "Path" Then
+                ListViewFiles.Columns.Add(PropName.Name, 0)
+                If PropName.Visible Then ListViewFiles.Columns.Item(ListViewFiles.Columns.Count - 1).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+            ElseIf PropName.Name = "Name" Then
+                If Not PropName.Visible Then
+                    ListViewFiles.Columns.Item(0).Width = 0
+                Else
+                    ListViewFiles.Columns.Item(0).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                End If
+            ElseIf PropName.Name = "Path" Then
+                If Not PropName.Visible Then
+                    ListViewFiles.Columns.Item(1).Width = 0
+                Else
+                    ListViewFiles.Columns.Item(1).AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+                End If
+            End If
+
+        Next
+
+
+
+
 
         FMain.Cursor = Cursors.Default
 
