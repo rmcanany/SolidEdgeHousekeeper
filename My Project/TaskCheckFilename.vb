0dc0fd263b9f1ee7378404d4227cb5f705e95f7e
rmcanany <64286960+rmcanany@users.noreply.github.com>
2025-04-07 16:30:57 -0500

    Documentation

--- My Project/TaskCheckFilename.vb
+++ My Project/TaskCheckFilename.vb
@@ -707,7 +707,7 @@ Public Class TaskCheckFilename
         HelpString += "Since draft files rarely have properties of their own, this is usually not necessary. "
         HelpString += "Also, because missing properties are reported as an error, "
         HelpString += "it can be distracting/confusing as well. "
-        HelpString += "One more thing on this option.  "
+        HelpString += "One more thing.  "
         HelpString += "In the example above, the program is set up to check if the model and draft files have the same name.  "
         HelpString += "With this option enabled, it would also check if the draft file has the same name as itself. "
         HelpString += "That will always be true, and never be what you intended. "
